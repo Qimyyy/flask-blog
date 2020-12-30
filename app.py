d73f978a0f8c29c7f490425990fa91ded8d51103
Artem 123 <koshax99@gmail.com>
2020-12-30 16:30:49 +0300

    Hello Word

--- app.py
+++ app.py
@@ -1,82 +1,127 @@
+import json
 import sqlite3
-from flask import Flask, render_template, url_for, request, flash, redirect
+from flask import Flask, render_template, url_for, request, flash, redirect, jsonify
 from werkzeug.exceptions import abort
+from flask_sqlalchemy import SQLAlchemy
 
-def get_db_connection():
-    conn = sqlite3.connect('database.db')
-    conn.row_factory = sqlite3.Row
-    return conn
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
-def get_post(post_id):
-    conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?',
-                        (post_id,)).fetchone()
-    conn.close()
-    if post is None:
-        abort(404)
-    return post
+from init_db import Post
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 app.config['SECRET_KEY'] = 'my_secret_key'
 
+engine = create_engine("sqlite:///database.db")
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
+
 @app.route('/')
+@app.route('/index')
 def index():
-    conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchall()
-    conn.close()
+    post1 = Post(title="Первый пост", text="Это первый пост на сайте")
+    session.add(post1)
+
+    posts = session.query(Post).all()
+    session.commit()
     return render_template('index.html', posts=posts)
 
-@app.route('/<int:post_id>')
-def post(post_id):
-    post = get_post(post_id)
-    return render_template('post.html', post=post)
 
-@app.route('/create', methods=('GET', 'POST'))
-def create():
+@app.route('/api/posts')
+def api_index():
+    posts = session.query(Post).all()
+
+    json_posts = []
+    for post in posts:
+        json_posts.append({
+            "id": post.id,
+            "text": post.text,
+            "title": post.title,
+            "date": post.date
+        })
+    session.commit()
+    return jsonify(json_posts)
+
+
+@app.route('/api/posts/<int:post_id>')
+def api_post(post_id):
+    post = session.query(Post).filter_by(id=post_id).one()
+    return jsonify({
+        "id": post.id,
+        "text": post.text,
+        "title": post.title,
+        "date": post.date
+    })
+    session.commit()
+
+
+@app.route('/posts/<int:post_id>')
+def post_detail(post_id):
+    one_post = Post.query.get_or_404(id=post_id)
+    return render_template('post.html', post=one_post)
+
+
+@app.route('/new', methods=('GET', 'POST'))
+def new_post():
     if request.method == 'POST':
-        title = request.form['title']
-        content = request.form['content']
+        title_request = request.form['title']
+        text_request = request.form['content']
 
-        if not title:
-            flash('Title is required!')
-        else:
-            conn = get_db_connection()
-            conn.execute('INSERT INTO posts (title, content) VALUES (?,?)', (title, content))
-            conn.commit()
-            conn.close()
-            flash('Succesfully post created!')
-            return redirect(url_for('index'))
+        if title_request == "" or text_request == "":
+            return redirect(url_for('new_post'))
+        post = Post.query.create(title=title_request, text=text_request)
+
+        session.add(post)
+        session.commit()
+        return redirect(url_for("index"))
+
     return render_template('create.html')
 
-@app.route('/<int:id>/edit', methods=('GET', 'POST'))
-def edit(id):
-    post = get_post(id)
+
+@app.route('/api/posts/new', methods=['POST'])
+def api_new_post():
+    if not request.json or "title" not in request.json or "text" not in request.json:
+        abort(400)
+    
+    title_request = request.json['title']
+    text_request = request.json['text']
+
+    if title_request == "" or text_request == "":
+        return redirect(url_for('api_index'))
+    post = Post.query.create(title=title_request, text=text_request)
+
+    session.add(post)
+    session.commit()
+    return redirect(url_for("api_index"))
+
+
+@app.route('/posts/<int:post_id>/edit', methods=('GET', 'POST'))
+def post_edit(post_id):
+    edited_post = Post.query.get_or_404(id=post_id)
 
     if request.method == 'POST':
         title = request.form['title']
-        content = request.form['content']
+        text = request.form['content']
 
-        if not title:
-            flash('Title is required!')
-        else:
-            conn = get_db_connection()
-            conn.execute('UPDATE posts SET title = ?, content = ?'
-                         ' WHERE id = ?',
-                         (title, content, id))
-            conn.commit()
-            conn.close()
-            return redirect(url_for('index'))
-    return render_template('edit.html', post=post)
+        if title == "" or text == "":
+            return redirect(url_for('post_edit'))
 
-@app.route('/<int:id>/delete', methods=('POST',))
-def delete(id):
-    post = get_post(id)
-    conn = get_db_connection()
-    conn.execute('DELETE FROM posts WHERE id = ?', (id,))
-    conn.commit()
-    conn.close()
-    flash('"{}" was successfully deleted!'.format(post['title']))
-    return redirect(url_for('index'))
+        edited_post.title = title
+        edited_post.text = text
+        return redirect(url_for('index'))
+    return render_template('edit.html', post=edited_post)
+
+
+@app.route('/posts/<int:post_id>/delete', methods=('POST',))
+def delete(post_id):
+    deleted_post = Post.query.get_or_404(id=post_id)
+    session.delete(deleted_post)
+    session.commit()
+    return redirect(url_for("index"))
+
+
+# api_delete - добавить
 
 if __name__ == '__main__':
     app.run(debug=True)
--- init_db.py
+++ init_db.py
@@ -1,19 +1,31 @@
-import sqlite3
+# для настройки баз данных
+import json
 
-connection = sqlite3.connect('database.db')
+from sqlalchemy import Column, ForeignKey, Integer, String, Text, Date
 
-with open('schema.sql') as f:
-    connection.executescript(f.read())
+# для определения таблицы и модели
+from sqlalchemy.ext.declarative import declarative_base, DeclarativeMeta
 
-cur = connection.cursor()
+# для создания отношений между таблицами
+from sqlalchemy.orm import relationship, session
 
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('First Post', 'Content for the first post')
-            )
+# для настроек
+from sqlalchemy import create_engine
+from datetime import datetime
 
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-            ('Second Post', 'Content for the second post')
-            )
+# создание экземпляра declarative_base
+Base = declarative_base()
 
-connection.commit()
-connection.close()
+
+# здесь создаем наши модели
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(100), nullable=False)
+    text = Column(Text, nullable=False)
+    date = Column(Date, default=datetime.utcnow)
+
+
+engine = create_engine('sqlite:///database.db')
+Base.metadata.create_all(engine)
